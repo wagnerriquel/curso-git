0e3db3119e926697a55839693571ae387e25821d
Wagner Riquel <wagner.limariquel@gmail.com>
2021-09-06 14:16:33 -0300

    Acrescentada um mensagem de bom dia

--- teste/Program.cs
+++ teste/Program.cs
@@ -7,7 +7,8 @@ namespace teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            Console.WriteLine("Bom dia!");
+
             Console.WriteLine();
         }
     }
