c898490836e398e5c4255344bc8045e6af595475
Wagner Riquel <wagner.limariquel@gmail.com>
2021-09-07 12:24:15 -0300

    Acrescentada a pergunta como voce esta hoje

--- teste/Program.cs
+++ teste/Program.cs
@@ -8,6 +8,7 @@ namespace teste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
 
             Console.WriteLine();
         }
